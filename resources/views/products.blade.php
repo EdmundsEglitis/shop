3b8f2a3404bc83a1463c45318b762aeaf13d0523
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-18 16:55:27 +0300

    shop push

---
+++ resources/views/products.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=0, initial-scale=1.0">
+    <title>Products</title>
+</head>
+<body>
+    <h1>Products</h1>
+    <main>
+      <article>
+        <h2>Doctor's Sausage</h2>
+        <img src="/images/sausage.jpg" alt="big sausage cake">
+        <p>This is my sausage cake. Have a bite to enjoy the best flavors</p>
+        <p>1.49 EUR</p>
+      </article>
+    </main>
+</body>
+</html>
